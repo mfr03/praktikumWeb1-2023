fcfa25aa2b41bc920102eafbece97a051ea87d12
maul <maulgantengmaks@gmail.com>
2023-05-25 21:56:36 +0700

    seperating each questions

--- minggu11/js2-2.js
+++ minggu11/js2-2.js
@@ -5,9 +5,9 @@ if(isPraktikan.toLowerCase() == "iya")
     var nama = prompt("Tuliskan nama Anda");
     var nim = prompt("Tuliskan NIM anda");
     var angkatan = prompt("Tuliskan tahun angkatan anda");
-
-    document.write("<table style='border: 1px solid black'>" + "<tr>" + "<td>" +  nama +  "</td>" + " " + "<td>" +  nim +  "</td>" + " " + "<td>" +  angkatan +  "</td>" + "</tr>" + "</table>");
+    document.write('<table style="border: 1px solid black;"><tr><th>' + nama + '</th><th>' + nim+'</th><th>' + angkatan + '</th></tr></table>');
 } 
+
 else if (isPraktikan.toLocaleLowerCase == "tidak")
 {
     document.write("Anda bukan praktikan PPW1, anda tidak boleh masuk! ")
